e98bd43ce1492a4797d5e8387ef7b82a6c002444
Eli Schleifer <1265982+EliSchleifer@users.noreply.github.com>
2023-05-31 08:55:38 -0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,6 @@ while [ $COUNT -gt 0 ]; do
     sleep 0.1
     let COUNT=COUNT-1
 done
-yes really interesting failure info is here | head -c 100KB > error.txt
 dd bs=1M count=10 if=/dev/urandom of=error.txt
 printf "${RED}See error.txt for more info"
 exit 1
