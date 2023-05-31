4b494498c6e2eb298f3043729ed9c8195941e130
Eli Schleifer <1265982+EliSchleifer@users.noreply.github.com>
2023-05-31 09:19:57 -0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,11 @@
 RED='\033[0;31m'
 COUNT=10
 while [ $COUNT -gt 0 ]; do
-    echo $COUNT
+    printf "$COUNT "
     sleep 0.1
     let COUNT=COUNT-1
 done
+printf "INFO: Test completed, 1 test FAILED, 10 total actions\n"
 dd bs=1M count=10 if=/dev/urandom of=error.txt status=none
 printf "${RED}See error.txt for more info\n"
 exit 1
