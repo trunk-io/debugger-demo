4d22067148dbe95a83ebd5a7b9d74c355b2ae8e5
pat-trunk-io <pat@trunk.io>
2023-09-28 13:42:27 -0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -11,3 +11,6 @@ printf "\nINFO: Test completed, 1 test FAILED, 10 total actions\n"
 dd bs=1M count=10 if=/dev/urandom of=error.txt status=none
 printf "${RED}See error.txt for more info\n"
 exit 1
+
+
+
